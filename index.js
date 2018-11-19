9b835f1386648001c0ffcac8af3066ccb8d41593
mahadev <mahadev@thoughtworks.com>
2018-11-19 10:36:18 +0530

    Hello

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
 var express = require('express')
+var request = require('superagent');
 var app = express()
 
+function myReq(response) {
+  new Promise((resolve, reject) => {
+    request
+      .put('https://www.eatonsecureconnect.com/m2m-eaton-web/rest/mobileUser/jwt/login')
+      .send({
+        "login": "USername",
+        "password": "somepassword"
+      })
+      .set('Content-Type', 'application/json')
+      .set('applicationId', 'a10a93111cc44bb4')
+      .end((err, res) => {
+        console.log("Response " + res.text + " Error" + err)
+        console.log(res.code)
+        resolve(res.text)
+      });
+  }).then((token) => {
+    console.log("Token.........",token)
+    response.send(token)
+  });
+}
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
-
-app.get('/', function(request, response) {
+app.get('/', function (request, response) {
   response.send('Hello World!')
 })
+app.get('/echo', function (request, response) {
+  token = myReq(response)
+  // response.send(token)
+})
 
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), function () {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
